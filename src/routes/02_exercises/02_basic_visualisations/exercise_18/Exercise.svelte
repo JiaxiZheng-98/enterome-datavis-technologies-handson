54cdb2880ece86390fc15e099270037876fd887c
JiaxiZheng-98 <jiaxihz@gmail.com>
2023-04-15 23:25:49 +0200

    18

--- src/routes/02_exercises/02_basic_visualisations/exercise_18/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_18/Exercise.svelte
@@ -1,18 +1,44 @@
+
+
 <script>
+  import {scaleLog} from 'd3-scale';
+  import {axisBottom} from 'd3-axis';
+  import {select} from 'd3-selection';
+
     // Dimensions
     const width = 800;
     const height = 100;
-    const margin = { top: 5, right: 5, bottom: 25, left: 5 };
+    const margin = { top: 5, right: 5, bottom: 5, left: 5 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
   
     // Array
     const values = [2, 4, 6, 7, 9];
+    const scale = scaleLog().
+                domain([2, 9])
+                .range([30, 500]);
+    
+    
+    
+  let xAxis;
+	$: {
+		select(xAxis).call(axisBottom(scale));
+	}
+
   </script>
   
   <svg viewBox="0 0 {width} {height}">
-    <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
+      {#each values as value}
+      <circle cx={scale(value)}
+              cy={value}
+              r=20
+              fill="red" />
+      {/each}
+
+    <g transform="translate(0, 80)"
+		 bind:this={xAxis}>
+        <text  x=250  y=20 fill="currentcolor">x Axis</text>
     </g>
+
   </svg>
-  
+
