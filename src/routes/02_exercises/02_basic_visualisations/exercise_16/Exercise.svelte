c2d407585641c7df6ad5717dd2af20af943882a4
JiaxiZheng-98 <jiaxihz@gmail.com>
2023-04-15 21:31:15 +0200

    16

--- src/routes/02_exercises/02_basic_visualisations/exercise_16/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_16/Exercise.svelte
@@ -1,17 +1,9 @@
-<script>
-    const width = 100;
-    const height = 100;
-    const margin = { top: 5, right: 5, bottom: 5, left: 5 };
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-  </script>
-  
-  <svg {width} {height}>
-    <g transform="translate({margin.left}, {margin.top})">
-      <circle cx={innerWidth / 2} cy={innerHeight / 2} r="20" fill="teal" />
-    </g>
+
+  <svg viewBox= "0,0,100,100">
+      <rect x="30%" y="30%" width="40%" height="40%" fill="yellow"/>
+      <circle cx="80%" cy="80%" r="20%"  fill="skyblue" />
   </svg>
-  
+
   <style>
     svg {
       border: 1px solid black;
