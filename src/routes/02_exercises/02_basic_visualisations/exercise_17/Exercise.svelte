f7347401e0bcadba4a4257953ac3a4c2a52ce9b5
JiaxiZheng-98 <jiaxihz@gmail.com>
2023-04-15 22:38:38 +0200

    17

--- src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_17/Exercise.svelte
@@ -1,4 +1,7 @@
+
+
 <script>
+  import { scaleLog } from 'd3-scale';
     // Dimensions
     const width = 800;
     const height = 100;
@@ -8,11 +11,21 @@
   
     // Array
     const values = [2, 4, 6, 7, 9];
+    const scale = scaleLog().
+                domain([2, 9])
+                .range([30, 500]);
+    
+
   </script>
   
   <svg viewBox="0 0 {width} {height}">
     <g transform="translate({margin.left},{margin.top})">
-      <!--  -->
+      {#each values as value}
+      <circle cx={scale(value)}
+              cy={value}
+              r=20
+              fill="blue" />
+      {/each}
     </g>
   </svg>
-  
+
